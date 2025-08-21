033804f7f3591a0f4354c7d217426b0ea3517e54
Abdul Hannan <lahama502@outlook.com>
2025-08-22 00:22:14 +0500

    Added ping and health check endpoints

--- src/server.js
+++ src/server.js
@@ -171,6 +171,8 @@ const PORT = process.env.PORT || 5000;
 const server = app.listen(PORT, () => {
   console.log(`Server is running on http://localhost:${PORT}`);
   console.log(`API documentation available at http://localhost:${PORT}/api-docs`);
+    console.log("Hello from this CId CD Pipeline");
+
 });
 
 // Handle unhandled promise rejections
