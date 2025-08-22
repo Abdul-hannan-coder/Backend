8b62d2f3f2beb1d188fb0038e15b91e76541f4bd
Abdul Hannan <lahama502@outlook.com>
2025-08-22 15:15:08 +0500

    cors setup

--- src/server.js
+++ src/server.js
@@ -93,13 +93,13 @@ const allowedOrigins = [
 //   origin:"*",
 //   credentials: true
 // }))
-// app.use(cors({
-//   origin: "http://localhost:5173", // your Vite frontend
-//   credentials: true
-// }));
+app.use(cors({
+  origin: "http://localhost:5173", // your Vite frontend
+  credentials: true
+}));
 
 
-app.use(cors())
+// app.use(cors())
 app.use(express.json({ limit: '10mb' }));
 app.use(express.urlencoded({ extended: true, limit: '10mb' }));
 
